36269f603b6e2088c94e167234d2865452c73e2f
Matthew Jackson <majackso@amgen.com>
2017-04-21 16:53:36 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##     The function makeCacheMatrix takes an object x and, so long as that object is a square 
+##   matrix or a single number, returns a list with two elements: 
+##      + The matrix x itself (or a one by one matrix containing x, if x was a single number)
+##      + A null object where the inverse may be inserted. 
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        if(!is.matrix(x) & (!is.numeric(x) | length(x) != 1)){
+                message("Error: object input to makeCacheMatrix is not a matrix") 
+                return(x)
+        }
+
+        if(is.matrix(x)){ 
+                if(dim(x)[1] != dim(x)[2]){
+                        message("Error: matrix input to makeCacheMatrix is not square") 
+                        return(x)
+                }
+        }
+
+        if(!is.matrix(x) & is.numeric(x) & length(x) == 1){
+                message("Warning: Number coerced to 1 by 1 matrix") 
+                x<-matrix(x)
+        }
+
+        i <- NULL
+
+        list(matrix=x, inverse = i)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##     The function cacheSolve takes an object of the form output by makeCacheMatrix.  It returns 
+##   errors if the input object has the incorrect form, or the form returned by makeCacheMatrix when
+##   it does not receive a square matrix.  
+##     It checks the second component of the list; if this is not null, then the function does nothing. 
+##   Otherwise, it checks to see if the input matrix is invertible, and if it is, it write the inverse of 
+##   that matrix into the second element of the list.  It does not repeat the checks made by 
+##   makeCacheMatrix to ensure that the first element of the input object is indeed a square matrix.
+
+
+cacheSolve <- function(x){
+
+        if(is.list(x)){
+                if(length(x)!=2 | !is.matrix(x[[1]])){
+                        message("Error: List input to cacheSolve has the wrong form")
+                        return(x)
+                }
+        }else{
+                message("Error: Object input to cacheSolve is not a list")
+                return(x)
+        }
+
+        if(!is.null(x[[2]])){
+                message("Using cached value of inverse")
+                return(x)
+        }else{
+                if(det(x[[1]])==0){
+                        message("Warning: Matrix is not invertible")
+                        x[[2]]<-"Inverse does not exist"
+                 }else{
+                         x[[2]]<-solve(x[[1]])
+
+                 }
+         }
+
+         return(x)
 }
+
+
